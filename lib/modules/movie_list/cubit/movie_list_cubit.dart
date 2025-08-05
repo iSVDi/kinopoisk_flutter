4fd5233fc08acdc93042653d375ba8baf58b9870
Daniil <d.shestopalov@letmecode.org>
2025-08-05 16:25:50 +0300

    added presenting movie list

--- lib/modules/movie_list/cubit/movie_list_cubit.dart
+++ lib/modules/movie_list/cubit/movie_list_cubit.dart
@@ -1,7 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:kinopoisk/modules/movie_list/models/movie_list_response.dart';
+import 'package:kinopoisk/services/network_service.dart';
 
 part 'movie_list_state.dart';
 
 class MovieListCubit extends Cubit<MovieListState> {
-  MovieListCubit() : super(MovieListState.loadingMovies);
+final NetworkService service = NetworkService.create();
+  List<Movie> movies = [];
+
+  MovieListCubit() : super(MovieListState.loadingMovies) {
+    _loadingMovies();
+  }
+
+  void _loadingMovies() async {
+    var res = await service.getMovies();
+    movies = MovieListResponse.fromJson(res.body).items;
+    emit(MovieListState.presentingMovies);
+  }
+  
 }
--- lib/modules/movie_list/movies_list_screen.dart
+++ lib/modules/movie_list/movies_list_screen.dart
@@ -2,28 +2,119 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kinopoisk/modules/movie_list/cubit/movie_list_cubit.dart';
 import 'package:kinopoisk/modules/movie_list/models/movie_list_response.dart';
-import 'package:kinopoisk/services/network_service.dart';
 
 class MoviesListScreen extends StatelessWidget {
-  final NetworkService service = NetworkService.create();
-
-  MoviesListScreen({super.key});
+  const MoviesListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<MovieListCubit, MovieListState>(
       builder: (context, state) {
-        return Center(
-          child: ElevatedButton(
-            onPressed: () async {
-              var res = await service.getMovies();
-              var castedRes = MovieListResponse.fromJson(res.body);
-              print(castedRes);
-            },
-            child: Text("start request"),
-          ),
-        );
+        switch (state) {
+          case MovieListState.presentingMovies:
+            return movieList(context);
+          case MovieListState.loadingMovies:
+            return loadingMovies();
+        }
       },
     );
+  }
+
+  Widget loadingMovies() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Movies is loading..."),
+          SizedBox(height: 10),
+          CircularProgressIndicator(),
+        ],
+      ),
+    );
+  }
+
+  Widget movieList(BuildContext context) {
+    var movies = context.read<MovieListCubit>().movies;
+
+    return ListView.separated(
+      itemBuilder: (context, id) {
+        return movieCell(movies[id]);
+      },
+      separatorBuilder: (_, _) {
+        return SizedBox(height: 20);
+      },
+      itemCount: movies.length,
+    );
+  }
+
+  Widget movieCell(Movie movie) {
+    var yearContries =
+        [movie.year.toString()] +
+        movie.countries.map((e) => e.country).toList();
+
+    const subtitleTextStyle = TextStyle(
+      color: Colors.grey,
+      fontSize: 15,
+      fontWeight: FontWeight.bold,
+    );
+    return Row(
+      children: [
+        Image.network(movie.posterUrl, width: 200, height: 200),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            spacing: 20,
+            children: [
+              Text(
+                movie.nameRu ?? movie.nameOriginal ?? "Name not provided",
+                maxLines: 2,
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+
+              Text(
+                movie.genres.map((e) => e.genre).toList().join(", "),
+                style: subtitleTextStyle,
+              ),
+
+              Text(yearContries.join(", "), style: subtitleTextStyle),
+              Row(
+                // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Expanded(flex: 2, child: SizedBox()),
+                  Text(
+                    movie.ratingKinopoisk.toString(),
+                    textAlign: TextAlign.end,
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.tealAccent,
+                      fontWeight: FontWeight.w800,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        Padding(padding: EdgeInsetsGeometry.only(left: 10)),
+      ],
+    );
+    // return Row(
+    //   children: [
+    //     Image.network(movie.posterUrl, width: 200, height: 200),
+
+    //     // Expanded(
+    //     //   child:
+    //     // ),
+
+    //     // Expanded(
+    //     //   child:
+    //     // ),
+    //   ],
+    // );
   }
 }
